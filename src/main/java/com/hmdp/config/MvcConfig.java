aadc3f4b51d7630784bd9770628d4251805872a3
Bug-Dever <514177676@qq.com>
2024-03-14 19:12:13 +0800

    Initial commit

---
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -0,0 +1,30 @@
+package com.hmdp.config;
+
+import com.hmdp.intercepter.LoginIntercepter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * ClassName: MvcConfig
+ * Package: com.hmdp.config
+ * Description:
+ *
+ * @Author Gush
+ * @Create 2024-03-14 16:41
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginIntercepter())
+                .excludePathPatterns(
+                        "/user/code",
+                        "/user/login",
+                        "/blog/hot",
+                        "/shop/**",
+                        "/shop-type/**",
+                        "/voucher/**"
+                );
+    }
+}
